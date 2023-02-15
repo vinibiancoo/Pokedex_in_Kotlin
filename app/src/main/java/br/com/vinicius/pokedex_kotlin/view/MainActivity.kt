cb160932fd05bd0210331ebfb307dad263e1786c
Vinicius Bianco Rodrigues <108638487+vinibiancoo@users.noreply.github.com>
2023-02-15 12:43:16 -0300

    Requisições com a api, ajustando erros e exibindo na tela os pokemons

--- app/src/main/java/br/com/vinicius/pokedex_kotlin/view/MainActivity.kt
+++ app/src/main/java/br/com/vinicius/pokedex_kotlin/view/MainActivity.kt
@@ -2,31 +2,35 @@ package br.com.vinicius.pokedex_kotlin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.vinicius.pokedex_kotlin.R
+import br.com.vinicius.pokedex_kotlin.api.PokemonRepository
 import br.com.vinicius.pokedex_kotlin.domain.Pokemon
 import br.com.vinicius.pokedex_kotlin.domain.PokemonType
+import br.com.vinicius.pokedex_kotlin.viewmodel.PokemonViewModel
+import br.com.vinicius.pokedex_kotlin.viewmodel.PokemonViewModelFactory
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val recyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rvPokemon)
+    }
+    private val viewModel by lazy {
+        ViewModelProvider(this, PokemonViewModelFactory())
+            .get(PokemonViewModel::class.java)
+    }
+    override fun onCreate(savedInstancesState: Bundle?){
+        super.onCreate(savedInstancesState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.rvPokemon)
-
-        val bulbassaur = Pokemon(
-            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-                1,
-                "Bulbassaur",
-                listOf(
-                    PokemonType("Fire")
-                )
-            )
-        val pokemons = listOf(bulbassaur, bulbassaur, bulbassaur, bulbassaur, bulbassaur)
-
-        val layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
+        viewModel.pokemons.observe(this, Observer {
+            loadRecyclerView(it)
+        })
+    }
+    private fun loadRecyclerView(pokemons: List<Pokemon?>){
+        recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = PokemonAdapter(pokemons)
     }
 }
--- app/src/main/java/br/com/vinicius/pokedex_kotlin/view/PokemonAdapter.kt
+++ app/src/main/java/br/com/vinicius/pokedex_kotlin/view/PokemonAdapter.kt
@@ -9,9 +9,10 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.vinicius.pokedex_kotlin.R
 import br.com.vinicius.pokedex_kotlin.domain.Pokemon
+import com.bumptech.glide.Glide
 
 class PokemonAdapter(
-    private val items: List<Pokemon>
+    private val items: List<Pokemon?>
 ) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -20,36 +21,36 @@ class PokemonAdapter(
         return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return items.size
-    }
+    override fun getItemCount() = items.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
 
-        holder.bindview(item)
+        holder.bindView(item)
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bindview(item: Pokemon) =  with(itemView){
+        fun bindView(item: Pokemon?) = with(itemView) {
             val ivPokemon = findViewById<ImageView>(R.id.ivPokemon)
-            val tvPokemonName = findViewById<TextView>(R.id.tvPokemonName)
-            val tvPokemonNum = findViewById<TextView>(R.id.tvPokemonNum)
+            val tvNumber = findViewById<TextView>(R.id.tvPokemonNum)
+            val tvName = findViewById<TextView>(R.id.tvPokemonName)
             val tvType1 = findViewById<TextView>(R.id.tvType1)
             val tvType2 = findViewById<TextView>(R.id.tvType2)
 
+            item?.let {
+                Glide.with(itemView.context).load(it.imageUrl).into(ivPokemon)
 
-            tvPokemonNum.text = "Nº ${item.number}"
-            tvPokemonName.text = item.name
-            tvType1.text = item.types[0].name
+                tvNumber.text = "Nº ${item.formattedNumber}"
+                tvName.text = item.formattedName
+                tvType1.text = item.types[0].name.capitalize()
 
-            if (item.types.size > 1){
-                tvType2.visibility = View.VISIBLE
-                tvType1.text = item.types[1].name
-            }else{
-                tvType2.visibility = View.GONE
-            }
+                if (item.types.size > 1) {
+                    tvType2.visibility = View.VISIBLE
+                    tvType2.text = item.types[1].name.capitalize()
+                } else {
+                    tvType2.visibility = View.GONE
+                }
             }
         }
-
     }
+}
